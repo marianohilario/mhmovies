dbdb7688bd8c6e2ddea4baaa97ab0b9246ee24bf
Mariano <marianohilario@gmail.com>
2024-05-03 01:11:12 -0300

    login page finished without footer

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,31 @@
-console.log(tempData);
+// console.log(tempData);
+
+
+
+
+
+
+
+
+
+
+
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach((input) => {
+   if (input.getAttribute('type') !== 'button') {
+     input.onfocus = () => {
+       input.previousElementSibling.classList.add('top', 'focus');
+       input.parentNode.classList.add('focus');
+     };
+     input.onblur = () => {
+       input.value = input.value.trim();
+       if (input.value.trim().length === 0) {
+         input.previousElementSibling.classList.remove('top');
+       }
+       input.previousElementSibling.classList.remove('focus');
+       input.parentNode.classList.remove('focus');
+     };
+   }
+});
