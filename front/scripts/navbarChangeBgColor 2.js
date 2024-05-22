bcfab774098c0592e6b3f671c7c1f0ccaf009e61
Mariano <marianohilario@gmail.com>
2024-05-22 10:25:51 -0300

    front/scripts/navbarChangeColor 2.js and front/scripts/showSearchInput 2.js deleted

--- front/scripts/navbarChangeBgColor 2.js
+++
@@ -1,9 +0,0 @@
-function navbarChangeBgColor() {
-    if (window.scrollY > 60) {
-        document.getElementById('navbar').classList.add('bgDark')
-    } else {
-        document.getElementById('navbar').classList.remove('bgDark')
-    }
-}
-
-module.exports = navbarChangeBgColor
