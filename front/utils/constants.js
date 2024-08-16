fb971f5d44680a243180ad615a44e53f381c7e82
marianohilario <marianohilario@gmail.com>
2024-08-16 09:26:45 -0300

    connecting front with back deployed url

--- front/utils/constants.js
+++ front/utils/constants.js
@@ -1,4 +1,5 @@
 // const url = 'https://api.1rodemayo.com/movies'
-const url = 'http://localhost:3000/movies'
+// const url = 'http://localhost:3000/movies'
+const url = "https://mhmovies-marianohilarios-projects.vercel.app/movies";
 
-module.exports = url
+module.exports = url;
