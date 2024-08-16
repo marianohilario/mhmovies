95e911f00df16f85400fd2ac95c867e22ad8f448
marianohilario <marianohilario@gmail.com>
2024-08-16 09:09:12 -0300

    index.js modified for vercel deployment

--- back/index.js
+++ back/index.js
@@ -1,12 +1,19 @@
 const app = require("./src/server.js");
 require('dotenv').config()
 const dbCon = require("./src/config/dbCon.js");
-PORT = process.env.PORT || 3000
+// PORT = process.env.PORT || 3000
 
 dbCon()
   .then((res) => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log(`Connected to the database`);
   })
   .catch((err) => {
     console.log("Error connecting to database");
   });
+// dbCon()
+//   .then((res) => {
+//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting to database");
+//   });
