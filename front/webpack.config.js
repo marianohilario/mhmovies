700056fba3bd89a6a68ca3c3876142092080f22f
marianohilario <marianohilario@gmail.com>
2024-10-01 15:24:07 -0300

    webpack modified

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,9 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
     entry: {
-        index: './scripts/index.js',
-        create: './scripts/createMovie.js'},
+        index: "./scripts/index.js",
+        create: "./scripts/createMovie.js",
+    },
     output: {
-        path: __dirname + '/public',
-        filename: '[name].js'
-    }
-}
+        path: path.resolve(__dirname + "/public"),
+        filename: "[name].js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./pages/sites.html", // Apunta a tu archivo HTML principal
+            filename: "index.html", // Nombre de salida
+            chunks: ["index"], // Asegúrate de que inserte el script correcto
+        }),
+        new HtmlWebpackPlugin({
+            template: "./pages/createMovie.html",
+            filename: "createMovie.html",
+            chunks: ["create"],
+        }),
+    ],
+};
