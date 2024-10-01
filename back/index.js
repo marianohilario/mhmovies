a683d5660ab8b63f5db19523ef4f9afd90607056
marianohilario <marianohilario@gmail.com>
2024-10-01 11:38:54 -0300

    Server loading issue fixed

--- back/index.js
+++ back/index.js
@@ -1,21 +1,23 @@
 const app = require("./src/server.js");
 require("dotenv").config();
 const dbCon = require("./src/config/dbCon.js");
-// PORT = process.env.PORT || 3000
+PORT = process.env.PORT || 3000;
 
+// dbCon()
+//     .then((res) => {
+//         console.log(res);
+//         console.log(`Connected to the database`);
+//     })
+//     .catch((err) => {
+//         console.log("Error connecting to database");
+//     });
 dbCon()
     .then((res) => {
-        console.log(`Connected to the database`);
+        console.log(res);
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
     .catch((err) => {
         console.log("Error connecting to database");
     });
-// dbCon()
-//   .then((res) => {
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((err) => {
-//     console.log("Error connecting to database");
-//   });
 
 module.exports = app;
